8b70c74d730727469c1a10d9b31d58d7bc6a43ba
Davide Albieni <d.albieni@astalegale.net>
2020-11-15 02:02:57 +0100

    first commit

---
+++ Mocks/WorkingWithAttributes.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UsingJakodevLibs.Mocks
+{
+    public class WorkingWithEnums
+    {
+        public Animals Animal {get;set;}
+    }
+
+    public enum Animals
+    {
+        [AnimalSounds("gibber")] apes = 1,
+        [AnimalSounds("bray")] asses,
+        [AnimalSounds("hum")] bees,
+        [AnimalSounds("drone")] beetles,
+        [AnimalSounds("growl")] bears,
+        [AnimalSounds("boom")] bitterns,
+        [AnimalSounds("whistle")] blackbirds,
+        [AnimalSounds("we speak of the“chick-chick” of the blackcap")] blackcaps,
+        [AnimalSounds("bellow")] bulls,
+        [AnimalSounds("grump")] camels,
+        [AnimalSounds("sing or quaver")] canaries,
+        [AnimalSounds("mew, purr, swear, and caterwaul")] cats,
+        [AnimalSounds("bleat and blear")] calves,
+        [AnimalSounds("chirp or pink")] chaffinches,
+        [AnimalSounds("pip")] chickens,
+        [AnimalSounds("sing")] cicadæ,
+        [AnimalSounds("crow")] cocks,
+        [AnimalSounds("moo or low")] cows,
+        [AnimalSounds("caw")] crows,
+        [AnimalSounds("cry, cuckoo")] cuckoos,
+        [AnimalSounds("bell")] deer,
+        [AnimalSounds("click")] dolphins,
+        [AnimalSounds("bray")] donkeys,
+        [AnimalSounds("bark, bay, howl, and yelp")] dogs,
+        [AnimalSounds("coo")] doves,
+        [AnimalSounds("quack")] ducks,
+        [AnimalSounds("scream")] eagles,
+        [AnimalSounds("trumpet")] elephants,
+        [AnimalSounds("chant")] falcons,
+        [AnimalSounds("buzz")] flies,
+        [AnimalSounds("bark and yelp")] foxes,
+        [AnimalSounds("croak")] frogs,
+        [AnimalSounds("cackle and hiss")] geese,
+        [AnimalSounds("we speak of the “merry twinkle” of the female")] goldfinch,
+        [AnimalSounds("chirp and pitter")] grasshoppers,
+        [AnimalSounds("we speak of the “drumming” of the grouse")] grouse,
+        [AnimalSounds("cry “come back”")] guineafowls,
+        [AnimalSounds("squeak")] guineapigs,
+        [AnimalSounds("squeak")] hares,
+        [AnimalSounds("scream")] hawks,
+        [AnimalSounds("cackle and cluck")] hens,
+        [AnimalSounds("neigh and whinny")] horses,
+        [AnimalSounds("laugh")] hyenas,
+        [AnimalSounds("howl")] jackals,
+        [AnimalSounds("chatter")] jays,
+        [AnimalSounds("mew")] kittens,
+        [AnimalSounds("baa and bleat")] lambs,
+        [AnimalSounds("sing")] larks,
+        [AnimalSounds("chuckle in their call")] linnets,
+        [AnimalSounds("roar")] lions,
+        [AnimalSounds("chatter")] magpies,
+        [AnimalSounds("squeak and squeal")] mice,
+        [AnimalSounds("chatter and gibber")] monkeys,
+        [AnimalSounds("pipe and warble")] nightingales,
+        [AnimalSounds("hoot and screech")] owls,
+        [AnimalSounds("low and bellow")] oxen,
+        [AnimalSounds("talk")] parrots,
+        [AnimalSounds("scream")] peacocks,
+        [AnimalSounds("coo")] pigeons,
+        [AnimalSounds("grunt, squeak, and squeal")] pigs,
+        [AnimalSounds("yelp")] puppies,
+        [AnimalSounds("chitter")] raccoons,
+        [AnimalSounds("croak")] ravens,
+        [AnimalSounds("chirp")] robins,
+        [AnimalSounds("whistle")] redstarts,
+        [AnimalSounds("caw")] rooks,
+        [AnimalSounds("screech or shriek")] screech_owls,
+        [AnimalSounds("baa or bleat")] sheep,
+        [AnimalSounds("hiss")] snakes,
+        [AnimalSounds("chirp or yelp")] sparrows,
+        [AnimalSounds("bellow and call")] stags,
+        [AnimalSounds("twitter")] swallows,
+        [AnimalSounds("cry")] swans,
+        [AnimalSounds("whistle")] thrushes,
+        [AnimalSounds("growl")] tigers,
+        [AnimalSounds("gobble")] turkey_cocks,
+        [AnimalSounds("scream")] vultures,
+        [AnimalSounds("hum")] whales,
+        [AnimalSounds("chirr")] whitethroats,
+        [AnimalSounds("howl")] wolves,
+
+    }
+
+    public class AnimalSounds : System.Attribute
+    {
+        private string _valueString;
+        public AnimalSounds(string sound)
+        {
+            _valueString = sound;
+        }
+
+        public string Sound
+        {
+            get
+            {
+                return _valueString;
+            }
+            internal set { _valueString = value; }
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using JakodevLibs.Collections;
+using JakodevLibs.Extensions;
+using UsingJakodevLibs.Mocks;
+
+using Enum = System.Enum;
+
+namespace UsingJakodevLibs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                //WorkingWithEnumsTest();
+                WorkingWithMatrixTest();
+
+
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+
+        }
+
+        private static void WorkingWithMatrixTest()
+        {
+            try
+            {
+                Console.WriteLine("Ciao, benvenuto nel test MATRIX:");
+                Console.WriteLine("Adesso verrà creata una matrice e stampata a video");
+                Console.WriteLine("Premere il tasto ANY per continuare...");
+                var v = Console.ReadLine();
+
+                var matrix = new DictionaryMatrix<DateTime, string, string>();
+                matrix.Add(new DateTime(2020,1,10), "Carl", "dentist");
+                matrix.Add(new DateTime(2020,1,11), "Carl", "exams");
+                matrix.Add(new DateTime(2020,1,12), "Carl", "travel");
+                matrix.Add(new DateTime(2020,1,13), "Carl", "travel");
+                matrix.Add(new DateTime(2020,1,14), "Carl", "home working");
+                matrix.Add(new DateTime(2020,1,15), "Carl", "home working");
+                matrix.Add(new DateTime(2020,1,16), "Carl", "day off");
+                matrix.Add(new DateTime(2020,1,17), "Carl", "customer meeting");
+                matrix.Add(new DateTime(2020,1,18), "Carl", "travel");
+                matrix.Add(new DateTime(2020,1,19), "Carl", "office");
+                matrix.Add(new DateTime(2020,1,10), "Mark", "office");
+                matrix.Add(new DateTime(2020,1,11), "Mark", "office");
+                matrix.Add(new DateTime(2020,1,12), "Mark", "doctor");
+                matrix.Add(new DateTime(2020,1,13), "Mark", "travel");
+                matrix.Add(new DateTime(2020,1,14), "Mark", "customer meeting");
+                matrix.Add(new DateTime(2020,1,15), "Mark", "presentation");
+                matrix.Add(new DateTime(2020,1,16), "Mark", "travel");
+                matrix.Add(new DateTime(2020,1,17), "Mark", "day off");
+                matrix.Add(new DateTime(2020,1,18), "Mark", "day off");
+                matrix.Add(new DateTime(2020,1,19), "Mark", "day off");
+                matrix.Add(new DateTime(2020,1,10), "July", "home working");
+                matrix.Add(new DateTime(2020,1,11), "July", "day off");
+                matrix.Add(new DateTime(2020,1,12), "July", "customer meeting");
+                matrix.Add(new DateTime(2020,1,13), "July", "dentist");
+                matrix.Add(new DateTime(2020,1,14), "July", "exams");
+                matrix.Add(new DateTime(2020,1,15), "July", "travel");
+                matrix.Add(new DateTime(2020,1,16), "July", "travel");
+                matrix.Add(new DateTime(2020,1,17), "July", "travel");
+                matrix.Add(new DateTime(2020,1,18), "July", "home working");
+                matrix.Add(new DateTime(2020,1,19), "July", "home working");
+                matrix.Add(new DateTime(2020,1,10), "Anne", "travel");
+                matrix.Add(new DateTime(2020,1,11), "Anne", "travel");
+                matrix.Add(new DateTime(2020,1,12), "Anne", "home working");
+                matrix.Add(new DateTime(2020,1,13), "Anne", "home working");
+                matrix.Add(new DateTime(2020,1,14), "Anne", "day off");
+                matrix.Add(new DateTime(2020,1,15), "Anne", "customer meeting");
+                matrix.Add(new DateTime(2020,1,16), "Anne", "travel");
+                matrix.Add(new DateTime(2020,1,17), "Anne", "office");
+                matrix.Add(new DateTime(2020,1,18), "Anne", "office");
+                matrix.Add(new DateTime(2020,1,19), "Anne", "office");
+
+                string utenti = String.Empty;
+                foreach (var col in matrix.ColsHeader)
+                {
+                    utenti += $"{col},";
+                }
+                Console.WriteLine($"Ora verranno stampati gli appuntamenti di {utenti} dal {matrix.RowsHeader.First().ToShortDateString()} al {matrix.RowsHeader.Last().ToShortDateString()}:" );
+                Console.WriteLine("");
+
+                Console.Write($"{FormatLenght(" ")}");
+                foreach (var col in matrix.ColsHeader)
+                {
+                    Console.Write($"{FormatLenght(col)}");
+                }
+
+                Console.WriteLine("");
+                foreach (var row in matrix.RowsHeader)
+                {
+                    Console.Write(FormatLenght(row.ToShortDateString()));
+                    foreach (var col in matrix.ColsHeader)
+                    {
+                        Console.Write($"{FormatLenght(matrix.GetValue(row, col))}");
+                    }
+                    Console.WriteLine("");
+                }
+
+
+
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("ERRORE: Lo vedi che sei un mona? ricontrolla quello che hai inserito");
+            }
+            finally
+            {
+                Console.WriteLine("");
+                Console.WriteLine("Premere il tasto ANY per chiudere...");
+                Console.ReadKey();
+            }
+        }
+
+        private static string FormatLenght(string s)
+        {
+            var spaces = new string(' ', 20);
+            s = s+spaces;
+            s = s.Substring(0, 20);
+            return s;
+        }
+        private static void WorkingWithEnumsTest()
+        {
+            try
+            {
+                Console.WriteLine("Ciao, benvenuto nel test Enums Extension:");
+                Console.WriteLine("inserisci un numero da 1 a 80");
+                var v = Console.ReadLine();
+
+
+                // WORKING WITH ENUM
+                var test = new WorkingWithEnums()
+                {
+                    Animal = (Animals)int.Parse(v)
+                };
+
+                string nomeAnimale = test.Animal.Name();
+                string versoAnimale = test.Animal.GetAttribute<AnimalSounds>().Sound;
+
+                Console.WriteLine($"You choose the {nomeAnimale}, and it sounds {versoAnimale}");
+                Console.WriteLine($"{nomeAnimale} >> is the enumerator");
+                Console.WriteLine($"{versoAnimale} >> is the Custom Data Annotation called: AnimalSounds");
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("ERRORE: Lo vedi che sei un mona? ricontrolla quello che hai inserito");
+            }
+            finally
+            {
+                Console.WriteLine("");
+                Console.WriteLine("Premere il tasto ANY per chiudere...");
+                Console.ReadKey();
+            }
+        }
+    }
+
+
+
+
+}
